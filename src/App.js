e9a00c5564a6c1e0990eb40c9b85838d0fd49585
Pluttooo96 <pustulm96@gmail.com>
2023-03-23 00:08:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+const [adviceObject, setAdviceObject] = useState('')
+const [width, setWidth] = useState(window.innerWidth > 960)
+const [loading, setLoading] = useState(true)
+ 
+const getAdvice = async() => {
+  setLoading(true)
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  const adviceObject = data.slip;
+  setAdviceObject(adviceObject)
+  setLoading(false)
+}
+
+const updateMedia = () => {
+  setWidth(window.innerWidth > 960);
+};
+
+useEffect(() => {
+  window.addEventListener("resize", updateMedia);
+  return () => window.removeEventListener("resize", updateMedia);
+});
+ 
+useEffect(() => {
+  getAdvice();
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='main__number'>ADVICE #{adviceObject.id}</div>
+      <div className='main__advice'>"{!loading ? adviceObject.advice : 'Loading...'}"</div>
+      <img className='main__divider' src={width ? './pattern-divider-desktop.svg' : './pattern-divider-mobile.svg'} alt='divider' />
+      <button className='main__button' onClick={() => getAdvice()}><img src='./icon-dice.svg' alt='dice ' /></button>
     </div>
   );
 }
